de35598dd4bf1aba185ff168b5b7bf7fc1a234ff
alvaro8888 <richardsiles1@gmail.com>
2020-12-18 13:10:28 -0400

    commit

---
+++ assest/script/index.js
@@ -0,0 +1,43 @@
+       // initialize scrollspy
+       $('body').scrollspy({
+
+           target: '.dotted-scrollspy'
+       });
+
+       // initialize lightbox
+       $(function() {
+
+           $("#mdb-lightbox-ui").load("../mdb-addons/mdb-lightbox-ui.html");
+       });
+
+       $('.navbar-collapse a').click(function() {
+
+           $(".navbar-collapse").collapse('hide');
+       });
+
+       /* WOW.js init */
+       new WOW().init();
+
+
+
+
+       function nuevo() {
+           console.log("yaaaaaaaaa");
+       }
+
+       /* metodo o funcion ver cliente, lo usare como evento de un boton */
+       function fcliente() {
+           document.getElementById("cliente").style["display"] = "block"; /* mostrar */
+           document.getElementById("supervisor").style["display"] = "none"; /* ocultar */
+           document.getElementById("caja").style["display"] = "none"; /* ocultar */
+           document.getElementById("producto").style["display"] = "none"; /* ocultar */
+
+       }
+
+       function fsupervisor() {
+           document.getElementById("cliente").style["display"] = "none"; /* ocultar */
+           document.getElementById("supervisor").style["display"] = "block"; /* mostrar */
+           document.getElementById("caja").style["display"] = "none"; /* ocultar */
+           document.getElementById("producto").style["display"] = "none"; /* ocultar */
+
+       }
